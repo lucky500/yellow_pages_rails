b51dd2aa00ebc7327eaaecae6d2c0f5966d4ea50
Guest User <Guest@epicodus-39.local>
2017-11-13 14:33:41 -0800

    allow user to edit category name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,45 @@ class CategoriesController < ApplicationController
     render :home
   end
 
+  def show
+    @category = Category.find(params[:id])
+    render :show
+  end
+
+  def new
+    @category = Category.new
+    render :new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   def about
 
+  end
+
+private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :categories
   root 'categories#home'
 
   get 'about', to: 'categories#about'
+
+  get 'show', to: 'categories#show'
+
+  get '/categories/new', to: 'categories#new'
 end
