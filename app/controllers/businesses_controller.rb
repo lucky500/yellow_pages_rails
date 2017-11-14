50ef5f33ac737040c6ef0d5ec59c1f59973db95b
Luciano Oliveira <luciano101@msn.com>
2017-11-13 21:37:49 -0800

    add edit and delete functionality

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -14,6 +14,38 @@ class BusinessesController < ApplicationController
     end
   end
 
+  def show
+    @category = Category.find(params[:category_id])
+    @business = @category.businesses.find(params[:id])
+
+    render :show
+  end
+
+  def edit
+    @category = Category.find(params[:category_id])
+    @business = @category.businesses.find(params[:id])
+
+    render :edit
+  end
+
+  def update
+    @category = Category.find(params[:category_id])
+    @business = @category.businesses.find(params[:id])
+    if @business.update(businesses_params)
+      redirect_to category_path(@business.category)
+    else
+      :edit
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:category_id])
+    @business = @category.businesses.find(params[:id])
+    if @business.destroy
+      redirect_to category_path(@business.category)
+    end
+  end
+
 private
   def businesses_params
     params.require(:business).permit(:name, :address, :city, :state, :zip, :phone_number, :website)
