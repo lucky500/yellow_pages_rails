95f6bb6f6f9e2b1958d0b42bb1878c12df938faa
Guest User <Guest@epicodus-39.local>
2017-11-13 19:43:36 -0800

    allow users to add business to categories

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -3,4 +3,19 @@ class BusinessesController < ApplicationController
     @category = Category.find(params[:category_id])
     @business = @category.businesses.new
   end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @business = @category.businesses.new(businesses_params)
+    if @business.save
+      redirect_to category_path(@business.category)
+    else
+      render :new
+    end
+  end
+
+private
+  def businesses_params
+    params.require(:business).permit(:name, :address, :city, :state, :zip, :phone_number, :website)
+  end
 end
